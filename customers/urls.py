566438096d014e861809303eea10edf782bbf9d6
Paulo David Córdoba Moreno <114004993+pcordoba-cafeto@users.noreply.github.com>
2023-12-07 15:44:59 -0500

    fix customer urls

--- customers/urls.py
+++ customers/urls.py
@@ -4,7 +4,7 @@ from rest_framework import routers
 from .views import CustomerRetrieveView, CustomerListView, CustomerBalanceView
 
 urlpatterns = [
-    path('customers/', CustomerListView.as_view(), name='customer-list'),
-    path('customers/<int:pk>/', CustomerRetrieveView.as_view(), name='customer-retrieve'),
-    path('customers/<int:pk>/balance/', CustomerBalanceView.as_view(), name='customer-balance'),
+    path('', CustomerListView.as_view(), name='customer-list'),
+    path('<int:pk>/', CustomerRetrieveView.as_view(), name='customer-retrieve'),
+    path('<int:pk>/balance/', CustomerBalanceView.as_view(), name='customer-balance'),
 ]
--- technical_test/urls.py
+++ technical_test/urls.py
@@ -46,13 +46,12 @@ schema_view = get_schema_view(
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    # path('api/', include('customers.urls')),  # Include your app's URLs
-    path('', include('customers.urls')),
+    path('customers/', include('customers.urls')),
     path('loans/', include('loans.urls')),
     path('payments/', include('payments.urls')),
 
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
-    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+    path('', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
 
 if settings.DEBUG:
